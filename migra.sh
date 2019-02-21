a1b3f0e68971ce4d21852cdb6ac21a813f83ecf9
Vinicius Vibrich <44097918+vvibrich@users.noreply.github.com>
2019-02-21 19:49:27 -0300

    Update migra.sh

--- migra.sh
+++ migra.sh
@@ -1,22 +1,27 @@
-sudo apt install aptitude
-aptitude install sshpass
-echo Host de Origem: 
-read hostO
-echo Usuario Origem: 
-read userO
-echo Senha de Origem: 
-read passO
-echo Host de Destino:
-read hostD
-echo Usuario de Destino: 
-read userD
-echo Senha de Destino: 
-read passD
+dialog --title --menu 0 0 0 Host-To-Host 'Migra de um server pro outro'
+pick=$( dialog --stdout --menu 'Deseja migrar conteúdo host-to-host?' 0 0 0   1 Sim 2 Não )
 
+case $pick in
+1)
+        #Origem
+        hostO=$( dialog --stdout --inputbox 'Digite o host de origem:' 0 0 )
+        userO=$( dialog --stdout --inputbox 'Digite o usuário de origem:' 0 0 )
+        passO=$( dialog --stdout --passwordbox 'Digite a senha de origem:' 0 0 )
+        content=$( dialog --stdout --inputbox 'Qual arquivo ou diretório quer migrar:' 0 0 )
+        #Destino
+        hostD=$( dialog --stdout --inputbox 'Digite o host de destino:' 0 0 )
+        userD=$( dialog --stdout --inputbox 'Digite o usuário de destino:' 0 0 )
+        passD=$( dialog --stdout --passwordbox 'Digite a senha de destino:' 0 0 )
+        destino=$( dialog --stdout --inputbox 'Digite para qual diretório quer migrar:' 0 0 )
+        #Onde a mágica acontece
+        sshpass -p $passO scp -r $userO@$hostO:~/$content .
+        sshpass -p $passD scp -r ./$content $userD@$hostD:~/$destino
+        rm -rf $content
+        dialog --title 'Migrou' --msgbox 'Migração concluida com sucesso' 6 40
+        clear
 
-sshpass -p $passO scp -r $userO@$hostO:~/www .
-echo  Baixou 
-sshpass -p $passD scp -r ./whois $userD@$hostD:~/www
-rm -rf www/
-echo Foi migrado
-
+;;
+2)
+        dialog --title 'Até mais' --msgbox 'Perfeito, até mais \o/' 6 40
+;;
+esac
